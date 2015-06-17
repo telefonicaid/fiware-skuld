bd2e282a1b844cca7979127b2078a0ef63c8cbc0
Fernando Lopez <fernando.lopezaguilar@telefonica.com>
2015-06-17 13:02:22 +0200

    Addind header to the files...

--- tests/test_expired_users.py
+++ tests/test_expired_users.py
@@ -1,3 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Telefónica Investigación y Desarrollo, S.A.U
+#
+# This file is part of FIWARE project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at:
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For those usages not covered by the Apache version 2.0 License please
+# contact with opensource@tid.es
+
 __author__ = 'fla'
 
 from unittest import TestCase
@@ -174,7 +197,7 @@ class TestExpiredUsers(TestCase):
         }
 
         # Extract the user_id from the request.path content
-        matchObj = re.match( r'/v3/users/(.*)', request.path, re.M|re.I)
+        matchObj = re.match(r'/v3/users/(.*)', request.path, re.M|re.I)
 
         return result[matchObj.group(1)]
 
