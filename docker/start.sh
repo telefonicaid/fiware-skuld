e67a3fc701b05f06d92b365b70e9672bf4f85318
visitor <henar.munozfrutos@telefonica.com>
2016-04-04 18:45:22 +0200

    update for xml reports

--- docker/start.sh
+++ docker/start.sh
@@ -4,4 +4,4 @@ sed -i -e "s/{KEYSTONE_IP}/${KEYSTONE_IP}/" common/configuration.py
 sed -i -e "s/{ADM_TENANT_ID}/${ADM_TENANT_ID}/" common/configuration.py
 
 export PYTHONPATH=../..
-behave features/ --tags ~@skip
+behave features/ --tags ~@skip --junit --junit-directory testreport
