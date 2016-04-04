e0292ff0046059459a48933509c628d4b98302cb
visitor <henar.munozfrutos@telefonica.com>
2016-04-04 18:01:23 +0200

    updating docker for skuld

---
+++ docker/start.sh
@@ -0,0 +1,7 @@
+sed -i -e "s/{ADM_TENANT_NAME}/${ADM_TENANT_NAME}/" common/configuration.py
+sed -i -e "s/{ADM_PASSWORD}/${ADM_PASSWORD}/" common/configuration.py
+sed -i -e "s/{KEYSTONE_IP}/${KEYSTONE_IP}/" common/configuration.py
+sed -i -e "s/{ADM_TENANT_ID}/${ADM_TENANT_ID}/" common/configuration.py
+
+export PYTHONPATH=../..
+behave features/ --tags ~@skip
